29aff36b05f1953b0dec036eb65988c713a093b1
Burness Scott III <bsiii102798@gmail.com>
2024-01-24 03:33:44 -0800

    schema/modles

---
+++ Main/controllers/homeRoutes.js
@@ -0,0 +1,68 @@
+const router = require('express').Router();
+const { Project, User } = require('../models');
+const withAuth = require.apply("../utils/auth")
+
+router.get('/', async (req, res) => {
+    try {
+
+        const projectData = await Project.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['name'],
+                },
+            ],
+        });
+
+        const projects = projectData.map((project) => project.get({ plain: true }));
+
+
+        res.render('homepage', {
+            projects,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+
+});
+
+router.get('/profile', withAuth, async (req, res) => {
+    try {
+        const userData = await User.findByPk(req.session.user_id, {
+            attributes: { exclude: ['password'] },
+            include: [{ model: Project }],
+        });
+
+        const user = userData.get({ plain: true });
+
+        res.render('profile', {
+            ...user,
+            logged_in: true
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+
+});
+
+router.get('login', (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect('/profile');
+        return;
+    }
+
+    res.render('login');
+});
+
+router.ger('checkout', (req, res) => {
+
+    res.render('checkout', { logged_in: req.session.logged_in })
+});
+
+router.get('/payment', async (req, res) => {
+
+    res.render("payment", { logged_in: req.session.logged_in })
+})
+
+module.exports = router;
