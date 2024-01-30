13d62908cd86b040bb62f0016ae41eac7a8bb833
Burness Scott III <bsiii102798@gmail.com>
2024-01-29 19:56:25 -0800

    view

--- Main/controllers/homeRoutes.js
+++ Main/controllers/homeRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { Project, User } = require('../models');
-const withAuth = require("../utils/auth")
+const withAuth = require("../utils/auth.js")
 
 router.get('/', async (req, res) => {
     try {
@@ -46,7 +46,7 @@ router.get('/profile', withAuth, async (req, res) => {
 
 });
 
-router.get('login', (req, res) => {
+router.get('/login', (req, res) => {
     if (req.session.logged_in) {
         res.redirect('/profile');
         return;
@@ -55,7 +55,7 @@ router.get('login', (req, res) => {
     res.render('login');
 });
 
-router.get('checkout', (req, res) => {
+router.get('/checkout', (req, res) => {
 
     res.render('checkout', { logged_in: req.session.logged_in })
 });
@@ -63,6 +63,62 @@ router.get('checkout', (req, res) => {
 router.get('/payment', async (req, res) => {
 
     res.render("payment", { logged_in: req.session.logged_in })
-})
+});
+
+router.get("/about", (req, res) => {
+    res.render("about")
+});
+
+router.get("/contact", (req, res) => {
+    res.render("contact")
+});
+
+router.get("/about", (req, res) => {
+    res.render("about")
+});
+
+router.get("/landscape", (req, res) => {
+    res.render("landscape")
+});
+
+
+
+router.get("/project", (req, res) => {
+    res.render("project", { logged_in: req.session.logged_in })
+});
+
+router.get("/rocky", (req, res) => {
+    res.render("rocky", { logged_in: req.session.logged_in })
+});
+
+router.get("/services", (req, res) => {
+    res.render("services")
+});
+
+router.get("/signup", (req, res) => {
+    res.render("signup")
+});
+
+router.get("/simple", (req, res) => {
+    res.render("simple", { logged_in: req.session.logged_in })
+});
+
+router.get("/teams", (req, res) => {
+    res.render("teams")
+});
+
+router.get("/tropical", (req, res) => {
+    res.render("tropical", { logged_in: req.session.logged_in })
+});
+
+router.get("/modern", (req, res) => {
+    res.render("modern", { logged_in: req.session.logged_in })
+});
+
+router.get("/info", (req, res) => {
+    res.render("info")
+});
+
+
 
 module.exports = router;
