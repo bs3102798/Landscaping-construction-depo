f8fafb705e995e5e918d90ebd8007d554af02237
Burness Scott III <bsiii102798@gmail.com>
2024-02-03 17:48:21 -0800

    htmlAdd/protfolioFile

---
+++ Main/public/js/tropical.js
@@ -0,0 +1,7 @@
+let iconCart = document.querySelector('.icon-cart');
+let closeCart = document.querySelector('.close');
+let body = document.querySelector('body');
+
+let listCart = document.querySelector('.listCart');
+
+let cart = [];
